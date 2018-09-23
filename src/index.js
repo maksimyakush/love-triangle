b2feef78ca7ff25b1a5fdc6f8c7e32219cd62683
Maksim Yakush <maksimo-1988@yandex.ru>
2018-09-23 18:17:57 +0300

    love-triangles/fix

--- src/index.js
+++ src/index.js
@@ -4,18 +4,11 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   let counter = 0;
-  let arr = [];  
   for (let i = 0; i < preferences.length; i++) {
-    let firstSpichonee = preferences[i] - 1;
-    let secondSpichonee = preferences[firstSpichonee] - 1;
-    let thirdSpichonee = preferences[secondSpichonee] - 1;
-    if (firstSpichonee != i && secondSpichonee != i && thirdSpichonee === i) {
-      let sum = firstSpichonee + secondSpichonee + thirdSpichonee;
-      if(!arr.includes(sum)) {
-        arr.push(sum);
-        counter++;
-      } 
-    }  
+    let firstSpichonee = preferences[i];
+    let secondSpichonee = preferences[firstSpichonee - 1];
+    let thirdSpichonee = preferences[secondSpichonee - 1];
+    if (firstSpichonee - 1 !== i && thirdSpichonee -1 === i) counter++;
   }
-  return counter;
+  return counter / 3;
 }
